f958f4cc2bd8fdd63a5a180fa17cbb44667d5de3
Harsh Kumar rattewal <harshrattewal@gmail.com>
2023-10-13 17:01:45 +0530

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import JobDetails from "./components/Home/JobDetails";
+import Header from "./components/Home/Header";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import AddJob from "./components/AddJob/AddJob";
+import EditJob from "./components/EditJob/EditJob";
+import Error404 from "./components/NotFound/Error404";
+import Health from "./components/Health/Health";
+import { Provider } from "./context/JobContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Home />
+                </>
+              }
+            />
+            <Route
+              path="/:id"
+              element={
+                <>
+                  <Header />
+                  <JobDetails />
+                </>
+              }
+            />
+            <Route path="/health" element={<Health />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/addJob" element={<AddJob />} />
+            <Route path="/editJob/:id" element={<EditJob />} />
+            <Route path="/404" element={<Error404 />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
